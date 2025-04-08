4248d78be3e6ed5af30626d041650d713e6a33c4
Jonas <jonathan.ortiz@instaleap.io>
2025-04-08 18:52:41 -0500

    rateLimiter middleware

---
+++ internal/infrastructure/middleware/rateLimiter.go
@@ -0,0 +1,45 @@
+package rateLimiter
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/redis/go-redis/v9"
+)
+
+var ctx = context.Background()
+
+const window = 60 * time.Second // minuto
+const limit = 10                // 10 request
+
+func RateLimiterMiddleware(redisClient *redis.Client) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		userIP := c.ClientIP()
+		
+		log.Println(c.Request.URL.Path)
+
+		countPerIP, err := redisClient.Incr(ctx, userIP).Result()
+
+		if err != nil {
+			log.Printf("Error when incrementing count per IP %s: %v", userIP, err)
+			c.AbortWithStatus(http.StatusInternalServerError)
+			return
+		}
+
+		if countPerIP == 1 {
+			redisClient.Expire(ctx, userIP, window).Err()
+		}
+
+		if countPerIP > limit {
+			log.Printf("Limit reached for IP %s", userIP)
+			c.AbortWithStatus(http.StatusTooManyRequests)
+			return
+		}
+
+		// pasa al siguiente middleware
+		c.Next()
+	}
+}
